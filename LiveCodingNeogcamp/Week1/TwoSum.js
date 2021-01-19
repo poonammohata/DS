cfa4ef1dadadc8ceae46ecc260e3a2a8800d513a
Poonammohata <poonammohata2@mail.com>
2021-01-19 14:38:26 +0530

    add new solution

--- LiveCodingNeogcamp/Week1/TwoSum.js
+++ LiveCodingNeogcamp/Week1/TwoSum.js
@@ -1,3 +1,5 @@
+// Solution 1:Runtime: 76 ms Memory Usage: 38.6 MB
+
 var twoSum = function(nums, target) {
     var output=[]
     for(let i=0;i<nums.length;i++){
@@ -11,3 +13,18 @@ var twoSum = function(nums, target) {
         }
     }    
 };
+
+//Solution 2:Runtime: 72 ms Memory Usage: 38.6 MB
+var twoSum=function(nums,target){
+    let start=0;
+    for(let i=1;i<nums.length;i++){
+        let sum=nums[start]+nums[i];
+        if(sum===target){
+            return [start,i]
+        }
+        else if(i==nums.length-1){
+            start++;
+            i=start;
+        }
+    }
+}
