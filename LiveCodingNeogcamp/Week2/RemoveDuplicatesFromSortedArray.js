025220ed91b68972f638ccf27e3c603ae3d0b3e4
Poonammohata <poonammohata2@mail.com>
2021-01-23 18:56:46 +0530

    add sol

---
+++ LiveCodingNeogcamp/Week2/RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,11 @@
+
+var removeDuplicates = function(nums) {
+    var i=0;
+     for(var j=1;j<nums.length;j++){
+         if(nums[i]!==nums[j]){
+             i++;
+             nums[i]=nums[j]
+         }
+     }
+     return i+1
+ };
